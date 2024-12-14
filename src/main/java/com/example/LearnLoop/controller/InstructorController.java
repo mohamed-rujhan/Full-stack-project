6a2cb3e9b0a78513bf5016f5ef5a27c67eefa1a0
Issadeen Mohamed Rujhan <rujhanim@Issadeens-MacBook-Air.local>
2024-12-14 19:24:06 +0530

    Added Swagger dependencies and updated InstructorController and Instructor classes

--- src/main/java/com/example/LearnLoop/controller/InstructorController.java
+++ src/main/java/com/example/LearnLoop/controller/InstructorController.java
@@ -1,6 +1,5 @@
 package com.example.LearnLoop.controller;
 
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.LearnLoop.model.Instructor;
 import com.example.LearnLoop.service.InstructorService;
-
 
 @RestController
 @RequestMapping({"/api/instructors"})
--- src/main/java/com/example/LearnLoop/model/Instructor.java
+++ src/main/java/com/example/LearnLoop/model/Instructor.java
@@ -3,9 +3,7 @@ package com.example.LearnLoop.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import com.mongodb.lang.NonNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
