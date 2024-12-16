5c397159194bebb55a1bdeb5d1363fb02966850f
Pirashanth003 <pira26604shanth4124885@gmail.com>
2024-12-17 00:07:42 +0530

    push payment

--- src/main/java/com/example/LearnLoop/model/Payment.java
+++ src/main/java/com/example/LearnLoop/model/Payment.java
@@ -1,26 +1,91 @@
 package com.example.LearnLoop.model;
 
-import java.security.Timestamp;
+import java.util.Date;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mongodb.lang.NonNull;
 
+@Document(collection = "payments")
 public class Payment {
-@Id
-private String payment_id;
-@Id
-private String student_id;
-@Id
-private String course_id;
-@NonNull
-private float amount;
-@NonNull
-private Timestamp payment_date;
-@NonNull
-private String status ;
-public void setPaymentId(String paymentId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setPaymentId'");
-}
+
+    @Id
+    private String id;
+    @Id
+    private String studentId;
+    @Id
+    private String courseId;
+    @Id
+    private String transactionId;
+    @NonNull
+    private String cardNumber;
+    @NonNull
+    private double amount;
+    @NonNull
+    private Date paymentDate;
+
+    public Payment(String studentId, String courseId, String transactionId, String cardNumber, double amount, Date paymentDate) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.transactionId = transactionId;
+        this.cardNumber = cardNumber;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+    
 }
