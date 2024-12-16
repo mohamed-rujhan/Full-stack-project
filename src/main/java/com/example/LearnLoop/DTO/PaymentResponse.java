fc4468018e26dcf6c9bd51f652667e9794694843
Pirashanth003 <pira26604shanth4124885@gmail.com>
2024-12-16 23:45:35 +0530

    push paymentresponse

---
+++ src/main/java/com/example/LearnLoop/DTO/PaymentResponse.java
@@ -0,0 +1,37 @@
+package com.example.LearnLoop.DTO;
+
+public class PaymentResponse {
+    private String status;
+    private String transactionId;
+    private String message;
+
+    public PaymentResponse(String status, String transactionId, String message) {
+        this.status = status;
+        this.transactionId = transactionId;
+        this.message = message;
+    }
+
+    // Getters and Setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
