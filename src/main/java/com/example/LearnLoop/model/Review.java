8f4618f82276c192cf1a547ed57e71cc5117ddcd
Issadeen Mohamed Rujhan <rujhanim@Issadeens-MacBook-Air.local>
2024-12-17 09:42:35 +0530

    fully completed instructor entity

--- src/main/java/com/example/LearnLoop/model/Review.java
+++ src/main/java/com/example/LearnLoop/model/Review.java
@@ -1,5 +1,29 @@
 package com.example.LearnLoop.model;
 
-public class Reviews {
+import java.security.Timestamp;
+import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.websocket.Decoder.Text;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "Reviews")
+public class Review {
+
+    private String reviewId;
+
+    private String student_id;
+
+    private String course_id;
+
+    private int rating;
+
+    private Text comment;
+
+    private Timestamp review_date;
+    
 }
+
