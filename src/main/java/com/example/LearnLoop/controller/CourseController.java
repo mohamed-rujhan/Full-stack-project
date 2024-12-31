b713e8cdb947e3ef5bf4b463f89e4f47ca9c247e
Pirashanth003 <pira26604shanth4124885@gmail.com>
2024-12-31 11:19:24 +0530

    push CourseControll

--- src/main/java/com/example/LearnLoop/controller/CourseController.java
+++ src/main/java/com/example/LearnLoop/controller/CourseController.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.LearnLoop.model.Courses;
+import com.example.LearnLoop.model.Course;
+
 import com.example.LearnLoop.service.CourseService;
 
 @RestController
@@ -16,30 +17,32 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-    public CourseController() {}
 
     @PostMapping
-    public ResponseEntity<Courses> createCourse(@RequestBody Courses course) {
-        Courses createdCourse = this.courseService.createCourse(course);
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = this.courseService.createCourse(course);
         return ResponseEntity.ok(createdCourse);
     }
 
+   
     @GetMapping("/{courseId}")
-    public ResponseEntity<Courses> getCourseById(@PathVariable String courseId) {
-        Courses course = this.courseService.getCourseById(courseId);
-        return course != null ? ResponseEntity.ok(course) : ResponseEntity.notFound().build();
+    public ResponseEntity<Course> getCourseById(@PathVariable String courseId) {
+        Course course = this.courseService.getCourseById(courseId);
+        return course != null ? ResponseEntity.ok(course) :
+            ResponseEntity.notFound().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<Courses>> getAllCourses() {
-        List<Courses> courses = this.courseService.getAllCourses();
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = this.courseService.getAllCourses();
         return ResponseEntity.ok(courses);
     }
 
     @PutMapping("/{courseId}")
-    public ResponseEntity<Courses> updateCourse(@PathVariable String courseId, @RequestBody Courses course) {
-        Courses updatedCourse = this.courseService.updateCourse(courseId, course);
-        return updatedCourse != null ? ResponseEntity.ok(updatedCourse) : ResponseEntity.notFound().build();
+    public ResponseEntity<Course> updateCourse(@PathVariable String courseId, @RequestBody Course course) {
+        Course updatedCourse = this.courseService.updateCourse(courseId, course);
+        return updatedCourse != null ? ResponseEntity.ok(updatedCourse) :
+         ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{courseId}")
