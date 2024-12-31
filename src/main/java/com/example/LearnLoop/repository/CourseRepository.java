8b5305f18d24f3e8f55c9e7ece994eef6d773338
Pirashanth003 <pira26604shanth4124885@gmail.com>
2024-12-31 11:12:09 +0530

    push CourseRepository

--- src/main/java/com/example/LearnLoop/repository/CourseRepository.java
+++ src/main/java/com/example/LearnLoop/repository/CourseRepository.java
@@ -2,9 +2,9 @@ package com.example.LearnLoop.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.example.LearnLoop.model.Courses;
+import com.example.LearnLoop.model.Course;
 
 
-public interface CourseRepository extends MongoRepository<Courses,String>{
+public interface CourseRepository extends MongoRepository<Course,String>{
 
 }
