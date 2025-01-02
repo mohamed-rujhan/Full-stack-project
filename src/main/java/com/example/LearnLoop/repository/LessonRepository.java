6a6b6f8925e5d34f3106e194029cb9ede34f9144
Pirashanth <143879455+Pirashanth003@users.noreply.github.com>
2025-01-02 13:41:45 +0530

    Delete src/main/java/com/example/LearnLoop/repository directory

--- src/main/java/com/example/LearnLoop/repository/LessonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.LearnLoop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.LearnLoop.model.Lesson;
-
-public interface LessonRepository extends MongoRepository<Lesson,String>  {
-
-}
--- src/main/java/com/example/LearnLoop/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.LearnLoop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.LearnLoop.model.Student;
-
-public interface StudentRepository extends MongoRepository<Student,String>  {
-
-}
