83ba868e672865385618040fddf857f45397243f
Issadeen Mohamed Rujhan <rujhanim@Issadeens-MacBook-Air.local>
2025-01-22 14:30:02 +0530

    updated config

--- src/main/java/com/example/LearnLoop/controller/InstructorController.java
+++ src/main/java/com/example/LearnLoop/controller/InstructorController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.example.LearnLoop.service.InstructorService;
 
 @RestController
 @RequestMapping({"/api/instructors"})
+@CrossOrigin(origins = "http://localhost:5173")
 public class InstructorController {
 
     @Autowired
