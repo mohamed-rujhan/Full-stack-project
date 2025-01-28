8545cab815e36388f7a2fc2140655d8a0f99ccc3
Pirashanth003 <pira26604shanth4124885@gmail.com>
2025-01-28 11:09:31 +0530

    InstructorRepository

--- src/main/java/com/example/LearnLoop/repository/InstructorRepository.java
+++ src/main/java/com/example/LearnLoop/repository/InstructorRepository.java
@@ -1,8 +1,11 @@
 package com.example.LearnLoop.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import com.example.LearnLoop.model.Instructor;
 
 public interface InstructorRepository extends MongoRepository<Instructor, String> {
-
+    @Query("{ 'userName': ?0, 'password': ?1 }")
+        Instructor findByUserNameAndPassword(String userName, String password);
 }
