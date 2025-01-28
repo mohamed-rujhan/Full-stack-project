bae481a58d80a63b310b21ae0e3299ccb6cd7a92
Pirashanth003 <pira26604shanth4124885@gmail.com>
2025-01-28 11:10:31 +0530

    StudentRepository

--- src/main/java/com/example/LearnLoop/repository/StudentRepository.java
+++ src/main/java/com/example/LearnLoop/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.example.LearnLoop.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.LearnLoop.model.Student;
 
 public interface StudentRepository extends MongoRepository<Student,String>  {
+    @Query("{ 'userName': ?0, 'password': ?1 }")
+    Student findByUserNameAndPassword(String userName, String password);
 
 }
